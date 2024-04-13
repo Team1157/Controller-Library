b74beddc2c2da4ffe52e013103c55d44ae1a1da6
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2024-04-12 23:57:52 -0600

    Make program stop if controller is disconnected ever
    previously if controller was not plugged in or got DC'd the read() function would send all 0s and not actually stop the program and tell the user the controller was disconnected... this has been fixed by checking if the controller monitor thread is alive or not

--- controller.py
+++ controller.py
@@ -1,7 +1,9 @@
 # code for controller input class
 from inputs import get_gamepad
+import inputs
 import math
 import threading
+import sys
 
 # class to retrieve Xbox controller input
 class XboxController(object):
@@ -38,74 +40,91 @@ class XboxController(object):
 
 
     def read(self): 
-        # return the button/trigger states
-        return {'a': self.A, 
-                'b': self.B,
-                'x': self.X,
-                'y': self.Y,
-                'ls': self.LeftThumb,
-                'rs': self.RightThumb,
-                'back': self.Back,
-                'start': self.Start,
-                'lb': self.LeftBumper,
-                'rb': self.RightBumper,
-                'dx': self.DPadX,
-                'dy': self.DPadY,
-                'lt': self.LeftTrigger,
-                'rt': self.RightTrigger,
-                'lsx': self.LeftJoystickX,
-                'lsy': self.LeftJoystickY,
-                'rsx': self.RightJoystickX,
-                'rsy': self.RightJoystickY}
+        if self._monitor_thread.is_alive():
+            # return the button/trigger states
+            return {'a': self.A, 
+                    'b': self.B,
+                    'x': self.X,
+                    'y': self.Y,
+                    'ls': self.LeftThumb,
+                    'rs': self.RightThumb,
+                    'back': self.Back,
+                    'start': self.Start,
+                    'lb': self.LeftBumper,
+                    'rb': self.RightBumper,
+                    'dx': self.DPadX,
+                    'dy': self.DPadY,
+                    'lt': self.LeftTrigger,
+                    'rt': self.RightTrigger,
+                    'lsx': self.LeftJoystickX,
+                    'lsy': self.LeftJoystickY,
+                    'rsx': self.RightJoystickX,
+                    'rsy': self.RightJoystickY}
+        else:
+            print("Closing Controller Library, Controller Monitor Thread Died.")
+            sys.exit(1)
+            
+            
 
 
     def _monitor_controller(self):
-        while True:
-            events = get_gamepad()
+        try:
+            while True:
+                try:
+                    inputs.get_gamepad()
+                    break
+                except inputs.UnpluggedError:
+                    print("Controller Disconnected")
 
-            # use event codes to set input values
-            for event in events:
-                #print(event.code + ": " + str(event.state)) # print code/value for debugging
-                if event.code == 'ABS_Y':
-                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_X':
-                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RY':
-                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_RX':
-                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
-                elif event.code == 'ABS_Z':
-                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'ABS_RZ':
-                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
-                elif event.code == 'BTN_TL':
-                    self.LeftBumper = event.state
-                elif event.code == 'BTN_TR':
-                    self.RightBumper = event.state
-                elif event.code == 'BTN_SOUTH':
-                    self.A = event.state
-                elif event.code == 'BTN_NORTH':
-                    self.X = event.state
-                elif event.code == 'BTN_WEST':
-                    self.Y = event.state
-                elif event.code == 'BTN_EAST':
-                    self.B = event.state
-                elif event.code == 'BTN_THUMBL':
-                    self.LeftThumb = event.state
-                elif event.code == 'BTN_THUMBR':
-                    self.RightThumb = event.state
-                elif event.code == 'BTN_SELECT':
-                    self.Back = event.state
-                elif event.code == 'BTN_START':
-                    self.Start = event.state
-                elif event.code == 'ABS_HAT0X':
-                    self.DPadX = event.state
-                elif event.code == 'ABS_HAT0Y':
-                    self.DPadY = event.state
+            
+            
+            while True:
+                events = inputs.get_gamepad()
+                # use event codes to set input values
+                for event in events:
+                    #print(event.code + ": " + str(event.state)) # print code/value for debugging
+                    if event.code == 'ABS_Y':
+                        self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    elif event.code == 'ABS_X':
+                        self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    elif event.code == 'ABS_RY':
+                        self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    elif event.code == 'ABS_RX':
+                        self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                    elif event.code == 'ABS_Z':
+                        self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                    elif event.code == 'ABS_RZ':
+                        self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                    elif event.code == 'BTN_TL':
+                        self.LeftBumper = event.state
+                    elif event.code == 'BTN_TR':
+                        self.RightBumper = event.state
+                    elif event.code == 'BTN_SOUTH':
+                        self.A = event.state
+                    elif event.code == 'BTN_NORTH':
+                        self.X = event.state
+                    elif event.code == 'BTN_WEST':
+                        self.Y = event.state
+                    elif event.code == 'BTN_EAST':
+                        self.B = event.state
+                    elif event.code == 'BTN_THUMBL':
+                        self.LeftThumb = event.state
+                    elif event.code == 'BTN_THUMBR':
+                        self.RightThumb = event.state
+                    elif event.code == 'BTN_SELECT':
+                        self.Back = event.state
+                    elif event.code == 'BTN_START':
+                        self.Start = event.state
+                    elif event.code == 'ABS_HAT0X':
+                        self.DPadX = event.state
+                    elif event.code == 'ABS_HAT0Y':
+                        self.DPadY = event.state
+        except:
+            print("Controller Disconnected, Please restart your program!")
 
 if __name__ == '__main__':
     import time
     controller = XboxController()
     while True:
         print(controller.read())
-        time.sleep(3)
+        time.sleep(1)
